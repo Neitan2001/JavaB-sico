1eb50b19d02e42aac0b139b721d7effb34e6686e
Natan Santana <natanns@ciandt.com>
2022-08-30 11:38:57 -0300

    Adição dos testes de variáveis

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,20 @@
+public class TestaVariaveis {
+
+    public static void main(String[] args) {
+        System.out.println("Olá novo teste");
+
+        int idade;
+        idade = 20;
+
+        System.out.println(idade);
+
+        idade = 20 + 10;
+
+        System.out.println(idade);
+
+        idade = 7 * 5 + 2;
+
+        System.out.println("A idade é " + idade + ", parabéns!");
+    }
+
+}
