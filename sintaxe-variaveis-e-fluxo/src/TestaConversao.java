8064741959c1b76713e45122b0dd7c6902b24b43
Natan Santana <natanns@ciandt.com>
2022-08-30 12:33:21 -0300

    adição do teste de conversão

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,18 @@
+public class TestaConversao {
+
+    public static void main(String[] args) {
+        double salario = 1270.50;
+
+        int valor = (int) salario; //casting
+        System.out.println(valor);
+
+        double valo1 = 0.2;
+        double valor2 = 0.1;
+
+        double total = valo1 + valor2;
+
+        System.out.println(total);
+
+        float pontoFlutuante = 3.14f;
+    }
+}
