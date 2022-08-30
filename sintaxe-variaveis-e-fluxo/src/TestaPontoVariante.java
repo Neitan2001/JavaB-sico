f5b51a0b56b24ba9b4d466e83413f7e01a3fa7cc
Natan Santana <natanns@ciandt.com>
2022-08-30 11:50:30 -0300

    Adição dos testes de pontos variantes

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoVariante.java
@@ -0,0 +1,21 @@
+public class TestaPontoVariante {
+
+    public static void main(String[] args) {
+        double salario;
+
+        salario = 35.5;
+
+        System.out.println("Meu salário é " + salario);
+
+        int divisao = 5/2;
+
+        System.out.println(divisao);
+
+        double outraDivisao = 5/2;
+        System.out.println(outraDivisao);
+
+        double maisUmaDivisao = 5.0/2;
+        System.out.println(maisUmaDivisao);
+    }
+
+}
